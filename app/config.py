4072ee522024ef30823467222fe3568f06f33ad1
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-04 21:57:13 +0600

    fix val

--- app/config.py
+++ app/config.py
@@ -1,12 +1,21 @@
 from pydantic_settings import BaseSettings
+from typing import Optional
 
 
 class Config(BaseSettings):
-    streamlit_title: str = "VideoLabelMagic"
-    models_directory: str = "models/"
-    output_directory: str = "outputs/"
-    object_class_directory: str = "object_class/"
-    default_frame_rate: float = 1.0
+    streamlit_title: Optional[str] = "VideoLabelMagic"
+    models_directory: Optional[str] = "models/"
+    output_directory: Optional[str] = "outputs/"
+    object_class_directory: Optional[str] = "object_class/"
+    default_frame_rate: Optional[float] = 1.0
+
+    # Object storage settings
+    storage_use_s3: Optional[bool] = False
+    s3_endpoint_url: Optional[str] = ""
+    s3_access_key: Optional[str] = ""
+    s3_secret_key: Optional[str] = ""
+    s3_bucket_name: Optional[str] = ""
+    s3_region_name: Optional[str] = "ru-1"
 
     class Config:
         env_file = ".env"
