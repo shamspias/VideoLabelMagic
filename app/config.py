8df7690e6e12a50942f416a11da038b380ef0735
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-04 11:15:26 +0600

    fix model namespace to models

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     streamlit_title: str = "VideoLabelMagic"
-    model_directory: str = "models/"
+    models_directory: str = "models/"
     output_directory: str = "outputs/"
     default_frame_rate: float = 1.0
 
