b067ca4c7594bb5a45be660b7cdf778e78b045c7
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-04 11:56:01 +0600

    object class into config

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Config(BaseSettings):
     streamlit_title: str = "VideoLabelMagic"
     models_directory: str = "models/"
     output_directory: str = "outputs/"
+    object_class_directory: str = "object_class/"
     default_frame_rate: float = 1.0
 
     class Config:
