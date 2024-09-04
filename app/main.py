cdb3d1a9118d9000ef7e985c0e37274e2b5d2ffa
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-04 11:46:18 +0600

    feat model conf

--- app/main.py
+++ app/main.py
@@ -16,13 +16,15 @@ model_selection = st.selectbox("Choose a model:", models)
 output_dir = st.text_input("Output directory", config.output_directory)
 frame_rate = st.number_input("Frame rate", value=config.default_frame_rate)
 
+model_confidence = st.number_input("Model Confidence", value=0.1)
+
 if st.button('Extract Frames'):
     if uploaded_file is not None:
         video_path = 'temp_video.mp4'
         with open(video_path, 'wb') as f:
             f.write(uploaded_file.getbuffer())
         extractor = VideoFrameExtractor(video_path, frame_rate, output_dir, model_selection)
-        extractor.extract_frames()
+        extractor.extract_frames(model_confidence)
         st.success('Extraction Completed!')
     else:
         st.error("Please upload a file to proceed.")
