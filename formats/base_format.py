697d4081700e1614452696148e8618f5623c93a7
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-04 14:19:26 +0600

    add base

--- formats/base_format.py
+++ formats/base_format.py
@@ -1,6 +1,24 @@
 class BaseFormat:
     def __init__(self, output_dir):
         self.output_dir = output_dir
+        self.image_dir = None
+        self.label_dir = None
 
-    def save_annotations(self, annotations):
-        raise NotImplementedError("This method should be implemented by subclasses.")
+    def ensure_directories(self):
+        """
+        Ensures that necessary directories are created.
+        """
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    def annotate_frame(self, frame, frame_path, frame_filename, model_conf, supported_classes):
+        """
+        Annotates the frame using the model output and saves the annotation in a format-specific manner.
+
+        Parameters:
+            frame (np.array): The frame to be annotated.
+            frame_path (str): Path where the frame image is saved.
+            frame_filename (str): Filename of the frame image.
+            model_conf (float): Model confidence threshold for annotations.
+            supported_classes (list): List of supported class names.
+        """
+        raise NotImplementedError("Subclasses should implement this method.")
