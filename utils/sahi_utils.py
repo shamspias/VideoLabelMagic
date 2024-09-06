a4422d10b7cef93b14fbcbd53cb0702c8a9ef6cf
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-06 23:42:51 +0600

    converter numpy

--- utils/sahi_utils.py
+++ utils/sahi_utils.py
@@ -1,5 +1,6 @@
 from sahi.predict import get_sliced_prediction
 from sahi import AutoDetectionModel
+import numpy as np
 
 
 class SahiUtils:
@@ -20,13 +21,40 @@ class SahiUtils:
         return detection_model
 
     def perform_sliced_inference(self, image):
-        # Perform sliced inference using the loaded model and SAHI
         results = get_sliced_prediction(
             image,
             self.model,  # this should be a sahi model
             slice_height=self.slice_size[0],
             slice_width=self.slice_size[1],
             overlap_height_ratio=self.overlap_ratio[0],
-            overlap_width_ratio=self.overlap_ratio[1]
+            overlap_width_ratio=self.overlap_ratio[1],
+            verbose=False
         )
-        return results
+        return self.format_predictions(results, image)
+
+    def format_predictions(self, prediction_result, image):
+        formatted_results = {"boxes": [], "names": {}, "orig_img": image, "orig_shape": image.shape, "path": "",
+                             "probs": None, "save_dir": None, "speed": None}
+        class_ids = set()
+        for prediction in prediction_result.object_prediction_list:
+            class_id = prediction.category.id
+            class_ids.add(class_id)
+            formatted_results["names"][class_id] = prediction.category.name
+            bbox_xyxy = [prediction.bbox.minx, prediction.bbox.miny, prediction.bbox.maxx, prediction.bbox.maxy]
+            formatted_results["boxes"].append({
+                "class_id": class_id,
+                "bbox": np.array(bbox_xyxy),
+                "score": prediction.score.value
+            })
+
+        formatted_results["boxes"] = self.convert_boxes(formatted_results["boxes"])
+        return formatted_results
+
+    def convert_boxes(self, boxes):
+        # Convert to ultralytics.engine.results.Boxes format or similar
+        # Ensure correct shape and concatenation of score and class_id
+        boxes_array = [np.concatenate([box["bbox"], [box["score"], box["class_id"]]]) for box in boxes]
+        if boxes_array:  # Check if list is not empty
+            return np.stack(boxes_array)  # Properly stack arrays to maintain structure
+        else:
+            return np.array([])  # Return an empty numpy array if no boxes
