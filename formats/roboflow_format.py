ca08cdbd478630277516623421828e2189b4dd51
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-07 13:36:11 +0600

    fixed with base class

--- formats/roboflow_format.py
+++ formats/roboflow_format.py
@@ -12,28 +12,13 @@ class RoboflowFormat(BaseFormat):
         os.makedirs(self.label_dir, exist_ok=True)
 
     def save_annotations(self, frame, frame_path, frame_filename, results, supported_classes):
-        """
-        Saves the annotations in the Roboflow specified format.
-        """
+        img_dimensions = frame.shape[:2]
+        annotations = self.process_results(frame, results, img_dimensions)
         annotation_filename = frame_filename.replace('.jpg', '.txt')
         annotation_path = os.path.join(self.label_dir, annotation_filename)
-        img_height, img_width = frame.shape[:2]
-
         with open(annotation_path, 'w') as f:
-            for result in results:
-                if hasattr(result, 'boxes') and result.boxes is not None:
-                    for box in result.boxes:
-                        class_id = int(box.cls[0])
-                        if supported_classes[class_id] in supported_classes:
-                            confidence = box.conf[0]
-                            xmin, ymin, xmax, ymax = box.xyxy[0]
-                            x_center = ((xmin + xmax) / 2) / img_width
-                            y_center = ((ymin + ymax) / 2) / img_height
-                            width = (xmax - xmin) / img_width
-                            height = (ymax - ymin) / img_height
-                            f.write(f"{class_id} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n")
-
-        # Generate metadata file if needed
+            for annotation in annotations:
+                f.write(annotation + "\n")
         self.create_data_yaml(supported_classes)
 
     def create_data_yaml(self, supported_classes):
