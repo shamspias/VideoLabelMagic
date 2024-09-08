068dc5aef7242ddc31025f737cfde85518d6ea2e
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-09-09 03:22:00 +0600

    fix write annotation roboflow

--- formats/roboflow_format.py
+++ formats/roboflow_format.py
@@ -1,6 +1,7 @@
-from formats.base_format import BaseFormat
 import os
 import yaml
+from typing import List
+from formats.base_format import BaseFormat
 
 
 class RoboflowFormat(BaseFormat):
@@ -10,6 +11,20 @@ class RoboflowFormat(BaseFormat):
         self.label_dir = os.path.join(output_dir, 'labels')
         os.makedirs(self.image_dir, exist_ok=True)
         os.makedirs(self.label_dir, exist_ok=True)
+
+    def write_annotations(self, frame_filename: str, annotations: List[str]):
+        """
+        Writes annotations to a file based on the frame filename.
+
+        Args:
+            frame_filename (str): The filename of the frame to which annotations relate.
+            annotations (List[str]): Annotations to be written to the file.
+        """
+        annotation_filename = frame_filename.replace('.jpg', '.txt')
+        annotation_path = os.path.join(self.output_dir, 'labels', annotation_filename)
+        with open(annotation_path, 'w') as file:
+            for annotation in annotations:
+                file.write(annotation + "\n")
 
     def save_annotations(self, frame, frame_path, frame_filename, results, supported_classes):
         img_dimensions = frame.shape[:2]
